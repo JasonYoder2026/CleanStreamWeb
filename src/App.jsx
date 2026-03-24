1f45c20be7c1ae82cf855e0e64cb67b387dc7844
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2026-03-24 12:11:30 -0400

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './styles/App.css'
+import TopBar from './components/topBar'
+import Login from "./components/LoginPage"
+import { useState, useRef, useEffect } from 'react'
+import SideBar from './components/SideBar'
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+
+
+function Login(){
+  return(<div>
+    <Link to="/Home">Hello</Link>
+  </div>)
+}
+
+function Home(){
+    const [showSideBar, setShowSideBar] = useState(false)
+  const sideBarRef = useRef(null)
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (sideBarRef.current && !sideBarRef.current.contains(event.target)) {
+        setShowSideBar(false)
+      }
+    }
+
+    if (showSideBar) {
+      document.addEventListener('mousedown', handleClickOutside)
+    }
 
     return () => document.removeEventListener('mousedown', handleClickOutside)
   }, [showSideBar])
