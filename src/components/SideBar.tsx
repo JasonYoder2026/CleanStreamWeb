1f295bab64d18c11da982f24b5038a39d02c1225
Jason Yoder <jason.yoder2@bsu.edu>
2026-03-24 15:47:35 -0400

    Fixes sidebar styling

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../styles/sideBar.css";
+import { Menu, ArrowBigLeft, Landmark, Map, Settings } from "lucide-react";
 
 function SideBar() {
   const [showSideBar, setShowSideBar] = useState(false);
@@ -7,17 +8,17 @@ function SideBar() {
     return (
       <div className="closed-side-bar">
         <div className="side-bar-button" onClick={() => setShowSideBar(true)}>
-          X
+          <Menu color="#000000"/>
         </div>
       </div>
     );
   } else {
     return (
       <div className="side-bar-container">
-        <div onClick={() => setShowSideBar(false)}>X</div>
-        <div>Refunds</div>
-        <div>Locations</div>
-        <div>Settings</div>
+        <div onClick={() => setShowSideBar(false)}><ArrowBigLeft color="#000000"/></div>
+        <div><Landmark color="#000000" />Refunds</div>
+        <div><Map color="#000000"/>Locations</div>
+        <div><Settings color="#000000"/>Settings</div>
       </div>
     );
   }
