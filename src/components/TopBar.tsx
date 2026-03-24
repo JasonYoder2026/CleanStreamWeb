2247f3fedd9fa40db8db3c6b01f990a932b77788
JamesR367 <JamesR3675@gmail.com>
2026-03-24 14:46:14 -0400

    Fix errors in top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,28 @@
-import { useEffect, useState } from 'react'
-import '../styles/topBar.css'
-import supabase from '../api/supabase-client'
-import HamburgerMenu from '../assets/hamburger-menu.svg'
+import { useEffect, useState } from "react";
+import "../styles/topBar.css";
+import supabase from "../api/supabase-client";
+import HamburgerMenu from "../assets/hamburger-menu.svg";
 
-function TopBar({setShowSideBar: any, sideBarRef }: { setShowSideBar: any, sideBarRef: React.RefObject<HTMLImageElement> }) {
-  const [userData, setUserData] = useState(null)
+interface topBarProps {
+  setShowSideBar: (prev: (prev: boolean) => boolean) => void;
+  sideBarRef: React.RefObject<HTMLImageElement>;
+}
+
+type userDataType = {
+  profiles: {
+    id: string;
+    full_name: string;
+    balance: number;
+    created_at: string;
+    refund_attempts: number;
+    notif_lead_time: number;
+    reward_tracker: number;
+    roles: string;
+  };
+};
+
+function TopBar({ setShowSideBar, sideBarRef }: topBarProps) {
+  const [userData, setUserData] = useState<userDataType | null>(null);
 
   useEffect(() => {
     fetchLocation();
@@ -13,24 +31,28 @@ function TopBar({setShowSideBar: any, sideBarRef }: { setShowSideBar: any, sideB
   const fetchLocation = async () => {
     const yourUuid = "bea49d86-0630-44a3-a6de-c192518215aa";
     const { data, error } = await supabase
-      .from('Location_to_Admin')
-      .select('profiles(*)').eq('user_id', yourUuid)
+      .from("Location_to_Admin")
+      .select("profiles(*)")
+      .eq("user_id", yourUuid);
 
-    if (error) console.error(error)
-    setUserData(data![0] as any)
+    if (error) console.error(error);
+    setUserData(data![0] as any);
   };
 
   const toggleSideBar = () => {
-    setShowSideBar(prev => !prev)
-  }
+    setShowSideBar((prev) => !prev);
+  };
 
   return (
     <div className="top-bar-container">
-      <div className='image-container'>
+      <div className="image-container">
         <img src="src\assets\Icon.png" alt="clean stream icon" />
-        <img src="src\assets\Slogan.png" alt="clean stream laundry solutions slogan" />
+        <img
+          src="src\assets\Slogan.png"
+          alt="clean stream laundry solutions slogan"
+        />
       </div>
-      <p className='admin-name'>Welcome: {userData?.profiles?.full_name}</p>
+      <p className="admin-name">Welcome: {userData?.profiles?.full_name}</p>
       <img
         ref={sideBarRef}
         onClick={toggleSideBar}
@@ -39,7 +61,7 @@ function TopBar({setShowSideBar: any, sideBarRef }: { setShowSideBar: any, sideB
         alt="menu"
       />
     </div>
-  )
+  );
 }
 
-export default TopBar
+export default TopBar;
