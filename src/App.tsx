b455e87f515d96af90a248ab3712e53da73a6280
JamesR367 <JamesR3675@gmail.com>
2026-03-26 11:11:16 -0400

    Create base locations page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./styles/App.css";
 import Login from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import RefundsPage from "./components/RefundsDashboardPage";
+import LocationsPage from "./components/LocationsDashboardPage";
 
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/home" element={<HomePage />}>
           <Route index element={<div>Dashboard</div>} />
           <Route path="refunds" element={<RefundsPage />} />
+          <Route path="locations" element={<LocationsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/LocationsDashboardPage.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import supabase from "../api/supabase-client";
+import "../styles/LocationsPage.css";
+import { Plus } from "lucide-react";
+
+interface locationinformation {
+  id: number;
+  Address: string;
+  Name: string;
+  longitude: number;
+  latitude: number;
+}
+
+function LocationsDashBoard() {
+  const [locationData, setLocationData] = useState<locationinformation[]>([]);
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  const fetchLocation = async () => {
+    const Uuid = "bea49d86-0630-44a3-a6de-c192518215aa";
+
+    // Get location IDs for this admin
+    const { data: adminData, error: adminError } = await supabase
+      .from("Location_to_Admin")
+      .select("location_id")
+      .eq("user_id", Uuid);
+
+    if (adminError) {
+      console.error(adminError);
+      return;
+    }
+
+    const locationIDs = adminData.map((row) => row.location_id);
+
+    //Fetch location names using their IDs
+    const { data: locations, error: locationsError } = await supabase
+      .from("Locations")
+      .select("*")
+      .in("id", locationIDs);
+
+    if (locationsError) {
+      console.error(locationsError);
+      return;
+    }
+
+    setLocationData(locations);
+  };
+
+  return (
+    <div>
+      <div className="top-section">
+        <select className="locations-list" name="Location List" id="">
+          {locationData.map((location, index) => (
+            <option key={index} value={location.Name}>
+              {location.Name}
+            </option>
+          ))}
+        </select>
+        <div className="sub-section">
+          <p>Add Location:</p>
+          <button>
+            <Plus className="plus" />
+          </button>
+        </div>
+      </div>
+
+      <div className="seperation-line"></div>
+      <div className="machine-description">
+        <p>Machines:</p>
+        <div className="sub-section">
+          <p>Add Machine:</p>
+          <button>
+            <Plus className="plus" />
+          </button>
+        </div>
+      </div>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default LocationsDashBoard;
