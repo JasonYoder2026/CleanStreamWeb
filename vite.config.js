73ff8cb2eb5d2b38f9b84af6b1e369ffd271d8a0
Jason Yoder <jason.yoder2@bsu.edu>
2026-03-27 15:19:59 -0400

    Adds RefundRepository test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, 
+    environment: 'jsdom', 
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'lcov'],
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['node_modules/', 'src/**/*.test.{ts,tsx}'],
+    },
+  },
 })
