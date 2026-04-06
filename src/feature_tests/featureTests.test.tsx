477a7873e51122aaf2caf0c1141944b9af0aad74
nolan-meyer1 <145584308+nolan-meyer1@users.noreply.github.com>
2026-04-06 10:43:19 -0400

    Refactored file name

---
+++ src/feature_tests/featureTests.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen, waitFor } from "@testing-library/react";
+
+
+describe('Feature Tests', () => {
+
+    describe("Refund Tests", () => {
+
+        it('Test that refund can get approved', () => {
+
+        });
+
+    });
+
+});
